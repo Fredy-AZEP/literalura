47230bab8a2783f6e7501d163a0623bfc8b4f493
Alfredo <alfredozepeta@gmail.com>
2024-05-12 04:00:30 -0600

    Initial Commit

---
+++ src/test/java/com/alurachallenge/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
